33dd47811e6c9cfdde9c91a6465940d1c396d3aa
Himanshu Parangat <martinmagni@gmail.com>
2021-08-11 00:37:39 -0700

    basic header and navbar done

--- local/app.js
+++ local/app.js
@@ -1,1 +1,11 @@
 console.log("hellow world");
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("navbartoggel").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("navbartoggel").style.width = "0";
+}
